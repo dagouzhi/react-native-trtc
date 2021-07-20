fb994d131a000582289f6ef26c17f8e40f8346c1
大狗吱 <dagouzhi@qq.com>
2021-07-20 11:53:47 +0800

    callci

--- ios/RNTRTC/RNTRTC.m
+++ ios/RNTRTC/RNTRTC.m
@@ -82,7 +82,7 @@ RCT_EXPORT_METHOD(enterRoom:(NSDictionary *)options scene:(int) scene){
     TRTCParams *param = [[TRTCParams alloc] init];
     param.sdkAppId = [options[@"sdkAppId"] intValue];
     param.userId = options[@"userId"];
-    param.channel = [options[@"strRoomId"] NSString];
+    param.strRoomId = [options[@"channel"] NSString];
     param.userSig = options[@"userSig"];
     if ([options objectForKey:@"privateMapKey"]) {
         param.privateMapKey = options[@"privateMapKey"];
